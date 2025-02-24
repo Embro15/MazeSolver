721b771c27da72d278020514e3641b2957730b4a
Embro15 <jesse.steinberg@mac.com>
2025-02-24 11:58:53 -0500

    L4: Drawing Some Lines

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from window import Window
+from point import Point, Line
 
 
 
@@ -6,6 +7,15 @@ from window import Window
 
 def main():
     win = Window(800, 600)
+
+    l = Line(Point(0,0), Point(800, 600))
+    l2 = Line(Point(0, 600), Point(800, 0))
+
+    win.draw_line(l, "RED")
+    win.draw_line(l2, "BLACK")
+
+
+
     win.wait_for_close()
 
 
